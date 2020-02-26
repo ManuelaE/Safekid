fd7e8766de16c11e4961d551bc0aa25981d66382
ManuelaE <42478774+ManuelaE@users.noreply.github.com>
2020-02-25 21:51:39 -0500

    Add files via upload
    first commit

---
+++ precache-manifest.636d2df1caf08bf7aab766da4f656034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ed6a97fa7a3c99834d1e4cc8345d94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "661dae8e0401b9786f8b",
+    "url": "/static/css/main.79862c61.chunk.css"
+  },
+  {
+    "revision": "a6977465b39014732fbf",
+    "url": "/static/js/2.32f2263e.chunk.js"
+  },
+  {
+    "revision": "7f84d11092c10d3a43893d78f68c23cb",
+    "url": "/static/js/2.32f2263e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661dae8e0401b9786f8b",
+    "url": "/static/js/main.bda4716a.chunk.js"
+  },
+  {
+    "revision": "40d04f001f0dd7d90ce2",
+    "url": "/static/js/runtime-main.852f99e7.js"
+  }
+]);
